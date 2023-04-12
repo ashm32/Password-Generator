b8d2d6d44a268603891a54282c1b23a58ca83944
ashm32 <127209262+ashm32@users.noreply.github.com>
2023-04-12 15:55:43 +0100

    Update script.js
    Done:
    -Asks user for their password criteria
    -Generates random values depending on user criteria 
    
    Need to:
    - validate the user inputs
    - add a function which which generate more than one random value 
    - if user inputs no then...
    - make use of getRandom function, maybe to generate multiple random values
    - takes the array of values which will make up the password and create a randomised output to be displayed in the generatePassword function

--- script.js
+++ script.js
@@ -13,12 +13,90 @@ var lowerCasedCharacters = [
 var upperCasedCharacters = [
   'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
- 
+  //create global variables for the user prompts
+  const passLength=0; 
+  const lowercase= "";
+  const uppercase= "";
+  const numeric= "";
+  const specialChar= "";
+  let generatedPass = [];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+   const passLength = prompt("Enter required password length, must be between 8-128"); 
+   const lowercase = prompt("Would you like to incorporate lowercase values? type yes or no"); 
+   const uppercase = prompt("Would you like to incorporate uppercase values? type yes or no"); 
+   const numeric = prompt("Would you like to incorporate numeric values? type yes or no"); 
+   const specialChar = prompt("Would you like to incorporate special characters ($@%&*, etc)? type yes or no"); 
+   //+ validate user input to check they've entered in the correct format
+
+  console.log("Password Length: " + passLength);
+  console.log("Include lowercase? " + lowercase);
+  console.log("Include uppercase? " + uppercase);
+  console.log("Include numbers? " + numeric);
+  console.log("Include special char? " + specialChar);
+
+//function can only return one value, so an array has been used to store all random values to be called collectively
+//get a random value and store it in generatedPass array
+  allRandom = [];
+  lowercaseRandom = lowerCasedCharacters[Math.floor(Math.random()*lowerCasedCharacters.length)];
+  upercaseRandom = upperCasedCharacters[Math.floor(Math.random()*upperCasedCharacters.length)];
+  numericRandom = numericCharacters[Math.floor(Math.random()*numericCharacters.length)];
+  specialCharRandom = specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+  allRandom.push(lowercaseRandom, upercaseRandom, numericRandom, specialCharRandom);
+  //console.log(allRandom);
+
+   // right now function hasn't been called, use to display final randomly generated password array
+   //Function for getting a random element from an array
 function getRandom(arr) {
+  return (allRandom); 
  }
+  
+ for (let i=0; i<passLength;){
+
+  if (lowercase=="yes"){
+    var randomLower = lowercaseRandom;
+console.log(randomLower);
+  generatedPass.push(randomLower);
+  console.log(generatedPass.length);
+  i++;
+  }
+   
+  if (uppercase=="yes"){
+    var randomUpper = upercaseRandom;
+  console.log(randomUpper);
+  generatedPass.push(randomUpper);
+  console.log(generatedPass.length);
+  i++;
+  }
+
+  if (numeric=="yes"){
+    var randomNumeric = numericRandom;
+ console.log(randomNumeric);
+  generatedPass.push(randomNumeric);
+  console.log(generatedPass.length);
+  i++;
+  }
+   
+  if (specialChar=="yes"){
+    var randomSpecialChar = specialCharRandom;
+  console.log(randomSpecialChar);
+  generatedPass.push(randomSpecialChar);
+  console.log(generatedPass.length);
+  i++;
+  }
+  console.log(i);
+}
+}
+
 
 // Function to generate password with user input
 function generatePassword() {
+  // check button is being clicked
+  console.log("Button clicked")
+// display the password by calling getRandom()
+return ("Generated password will be displayed here")
+
 }
 
 // Get references to the #generate element
